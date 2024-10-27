4d22be831dadec0a7316554404dfaef3f043f5b1
Gabriel Guimarães <gabsvander@hotmail.com>
2024-10-27 16:59:26 -0300

    #2 Add logging implementation

--- lib/core/logging/b_stop_logger_factory.dart
+++ lib/core/logging/b_stop_logger_factory.dart
@@ -1,5 +1,6 @@
 import 'package:b_stop/core/logging/b_stop_logger.dart';
+import 'package:b_stop/core/logging/impl/b_stop_logger_source_horizon_impl.dart';
 
 abstract class BStopLoggerFactory {
-  static BStopLogger standard() => throw UnimplementedError();
+  static BStopLogger standard() => BStopLoggerSourceHorizonImpl();
 }
