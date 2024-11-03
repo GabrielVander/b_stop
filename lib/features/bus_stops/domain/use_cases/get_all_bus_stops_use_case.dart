77a7624c501bde1ee1ac8e2ac37dc5d1a88335c9
Gabriel Guimarães <gabsvander@hotmail.com>
2024-11-03 17:25:35 -0300

    Re-introduce Stop entity

--- lib/features/bus_stops/domain/use_cases/get_all_bus_stops_use_case.dart
+++ lib/features/bus_stops/domain/use_cases/get_all_bus_stops_use_case.dart
@@ -2,7 +2,7 @@ import 'package:b_stop/core/logging/b_stop_logger.dart' show BStopLogger;
 import 'package:b_stop/core/logging/b_stop_logger_factory.dart' show BStopLoggerFactory;
 import 'package:b_stop/features/bus_stops/domain/dtos/stop_output.dart' show StopOutput;
 import 'package:b_stop/features/bus_stops/domain/repositories/stop_repository.dart' show StopRepository;
-import 'package:rust_core/rust_core.dart' show FutureResult, FutureResultExtension, Iter;
+import 'package:rust_core/rust_core.dart' show FutureResult, FutureResultExtension, Iter, Ok;
 
 class GetAllBusStopsUseCase {
   GetAllBusStopsUseCase({required StopRepository stopRepository}) : _stopRepository = stopRepository;
@@ -10,9 +10,12 @@ class GetAllBusStopsUseCase {
   final BStopLogger _logger = BStopLoggerFactory.standard();
   final StopRepository _stopRepository;
 
-  FutureResult<Iter<StopOutput>, String> call() async {
-    _logger.info('Getting all bus stops...');
-
-    return _stopRepository.fetchAll().inspectErr(_logger.warning).mapErr((_) => 'Unable to get all stops');
-  }
+  FutureResult<Iter<StopOutput>, String> call() async =>
+      FutureResult.value(const Ok<String, String>('Getting all bus stops...'))
+          .inspect(_logger.info)
+          .andThen((_) => _stopRepository.fetchAll())
+          .inspect((s) => _logger.info('Retrieved ${s.count} stops'))
+          .map((s) => s.map(StopOutput.fromStopEntity))
+          .inspectErr(_logger.warning)
+          .mapErr((_) => 'Unable to get all stops');
 }
--- test/features/bus_stops/domain/use_cases/get_all_bus_stops_use_case_test.dart
+++ test/features/bus_stops/domain/use_cases/get_all_bus_stops_use_case_test.dart
@@ -1,7 +1,8 @@
 import 'package:b_stop/features/bus_stops/domain/dtos/stop_output.dart' show StopOutput;
+import 'package:b_stop/features/bus_stops/domain/entities/stop.dart' show Stop;
 import 'package:b_stop/features/bus_stops/domain/repositories/stop_repository.dart' show StopRepository;
 import 'package:b_stop/features/bus_stops/domain/use_cases/get_all_bus_stops_use_case.dart' show GetAllBusStopsUseCase;
-import 'package:flutter_test/flutter_test.dart' show expect, test;
+import 'package:flutter_test/flutter_test.dart' show equals, expect, test;
 import 'package:latlong2/latlong.dart' show LatLng;
 import 'package:mocktail/mocktail.dart' show Mock, when;
 import 'package:rust_core/rust_core.dart' show Err, IterableExtension, Ok;
@@ -19,25 +20,35 @@ void main() {
     expect(result.unwrapErr(), 'Unable to get all stops');
   });
 
-  for (final stops in [
-    <StopOutput>[],
-    [const StopOutput(id: '8436EB2D-CAEA-4958-95E8-4A06C6AC3DF2', name: 'function', postion: LatLng(58.95, 99.53))],
-    [
-      const StopOutput(id: '4868D809-D8B3-4381-9616-CC6C656A4417', name: 'bunny', postion: LatLng(3.53, -41.34)),
-      const StopOutput(id: '387D948E-EAA7-4DF8-AA3E-2D92923C78E5', name: 'staying', postion: LatLng(-36.75, 54.12)),
-      const StopOutput(id: 'C8B05469-E579-4340-9B30-F1A6E7D5840B', name: 'variable', postion: LatLng(-34.93, -2.41)),
-    ]
+  for (final testCase in [
+    (<Stop>[], <StopOutput>[]),
+    (
+      [const Stop(id: '8436EB2D-CAEA-4958-95E8-4A06C6AC3DF2', name: 'function', lat: 58.95, lon: 99.53)],
+      [const StopOutput(id: '8436EB2D-CAEA-4958-95E8-4A06C6AC3DF2', name: 'function', postion: LatLng(58.95, 99.53))]
+    ),
+    (
+      [
+        const Stop(id: '4868D809-D8B3-4381-9616-CC6C656A4417', name: 'bunny', lat: 3.53, lon: -41.34),
+        const Stop(id: '387D948E-EAA7-4DF8-AA3E-2D92923C78E5', name: 'staying', lat: -36.75, lon: 54.12),
+        const Stop(id: 'C8B05469-E579-4340-9B30-F1A6E7D5840B', name: 'variable', lat: -34.93, lon: -2.41),
+      ],
+      [
+        const StopOutput(id: '4868D809-D8B3-4381-9616-CC6C656A4417', name: 'bunny', postion: LatLng(3.53, -41.34)),
+        const StopOutput(id: '387D948E-EAA7-4DF8-AA3E-2D92923C78E5', name: 'staying', postion: LatLng(-36.75, 54.12)),
+        const StopOutput(id: 'C8B05469-E579-4340-9B30-F1A6E7D5840B', name: 'variable', postion: LatLng(-34.93, -2.41)),
+      ]
+    ),
   ]) {
-    test('given stop repository returns $stops, then should return Ok', () async {
+    test('given stop repository returns ${testCase.$1}, then should return Ok(${testCase.$2})', () async {
       final mockStopRepository = _MockStopRepository();
       final useCase = GetAllBusStopsUseCase(stopRepository: mockStopRepository);
 
-      when(mockStopRepository.fetchAll).thenAnswer((_) async => Future.value(Ok(stops.iter())));
+      when(mockStopRepository.fetchAll).thenAnswer((_) async => Future.value(Ok(testCase.$1.iter())));
 
       final result = await useCase.call();
 
       expect(result.isOk(), true);
-      expect(result.unwrap(), stops.iter());
+      expect(result.unwrap(), equals(testCase.$2.iter()));
     });
   }
 }
