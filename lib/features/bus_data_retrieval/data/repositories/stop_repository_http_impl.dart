733ff13dd56f0bf650e5ed0e0b6d193a3ccd699a
Gabriel Guimarães <gabsvander@hotmail.com>
2024-11-13 23:58:02 -0300

    Simplify stops endpoint

--- lib/features/bus_data_retrieval/data/repositories/stop_repository_http_impl.dart
+++ lib/features/bus_data_retrieval/data/repositories/stop_repository_http_impl.dart
@@ -1,13 +1,11 @@
-import 'package:b_stop/core/logging/b_stop_logger.dart' show BStopLogger;
-import 'package:b_stop/core/logging/b_stop_logger_factory.dart' show BStopLoggerFactory;
-import 'package:b_stop/core/utils/cast.dart' show cast;
-import 'package:b_stop/features/bus_data_retrieval/data/models/stop_http_model.dart' show StopHttpModel;
-import 'package:b_stop/features/bus_stops/domain/dtos/stop_output.dart' show Stop;
+import 'package:b_stop/core/logging/b_stop_logger.dart';
+import 'package:b_stop/core/logging/b_stop_logger_factory.dart';
+import 'package:b_stop/core/utils/cast.dart';
+import 'package:b_stop/features/bus_data_retrieval/data/models/stop_http_model.dart';
 import 'package:b_stop/features/bus_stops/domain/entities/stop.dart';
-import 'package:b_stop/features/bus_stops/domain/repositories/stop_repository.dart' show StopRepository;
-import 'package:dio/dio.dart' show Dio, Options, Response;
-import 'package:rust_core/rust_core.dart'
-    show Err, FutureResult, FutureResultExtension, Iter, IterableExtension, Ok, Result;
+import 'package:b_stop/features/bus_stops/domain/repositories/stop_repository.dart';
+import 'package:dio/dio.dart';
+import 'package:rust_core/rust_core.dart';
 
 class StopRepositoryHttpImpl implements StopRepository {
   StopRepositoryHttpImpl(
@@ -34,8 +32,7 @@ class StopRepositoryHttpImpl implements StopRepository {
 
   FutureResult<Response<Object?>, String> _makeHttpRequest() async => Ok<Response<Object?>, String>(
         await _dioClient.get<Object>(
-          _stopsEndpoint.buildFullUrl(),
-          queryParameters: _stopsEndpoint.buildQueryParameters(),
+          _stopsEndpoint.url,
           options: Options(validateStatus: (_) => true),
         ),
       ).inspect((r) => _logger.debug('Received response: $r'));
@@ -79,43 +76,12 @@ class StopRepositoryHttpImpl implements StopRepository {
   Stop _parseAsStopEntity(Map<String, dynamic> json) => StopHttpModel.fromJson(json).toEntity();
 }
 
-final class HttpBaseInformation {
-  HttpBaseInformation({
-    required this.baseUrl,
-    required this.projectId,
-    required this.projectHash,
-  });
+sealed class HttpEndpoint {
+  HttpEndpoint({required this.url});
 
-  final String baseUrl;
-  final String projectId;
-  final String projectHash;
-}
-
-abstract interface class UrlBuilder {
-  String buildFullUrl();
-}
-
-abstract interface class QueryParametersBuilder {
-  Map<String, dynamic> buildQueryParameters();
-}
-
-sealed class HttpEndpoint implements UrlBuilder, QueryParametersBuilder {
-  HttpEndpoint({required this.httpBaseInfo, required this.endpoint});
-
-  final HttpBaseInformation httpBaseInfo;
-  final String endpoint;
+  final String url;
 }
 
 final class HttpStopsEndpoint extends HttpEndpoint {
-  HttpStopsEndpoint({required super.httpBaseInfo, required super.endpoint});
-
-  @override
-  String buildFullUrl() {
-    return '${httpBaseInfo.baseUrl}$endpoint';
-  }
-
-  @override
-  Map<String, dynamic> buildQueryParameters() {
-    return {'project_hash': httpBaseInfo.projectHash};
-  }
+  HttpStopsEndpoint({required super.url});
 }
